4ea651f58fc3b9809b08deb1b4cb2e0ab693795f
Cloud User <centos@ip-172-31-8-200.ec2.internal>
2017-12-14 07:41:39 +0000

    added warranty conditions

--- genconf/lib/best_low_opt.php
+++ genconf/lib/best_low_opt.php
@@ -9,6 +9,9 @@ while($ids = mysqli_fetch_array($id)){$idd[] = $ids;} //echo $idd[0]['id'];//var
 $array = array(); $conds="";
 for ($x = 0; $x <=$lastid[0]-1; $x++) 
 {
+	$conds_query="SELECT id FROM notebro_db.WAR WHERE FIND_IN_SET(id,(SELECT warranty FROM notebro_db.MODEL WHERE id=".$idd[$x]['id']."))>0 ORDER BY rating ASC LIMIT 1";
+	$cond["war"]=mysqli_fetch_row(mysqli_query($con,$conds_query))[0]; $conds="AND war=".$cond["war"]." ";
+	
 	$query= "SET @p0 = 'all_conf_".$idd[$x]['id']."'; SET @p1='".$idd[$x]['id']."'; SET @p2='".$conds."';";
 	$query.= "CALL optimal_conf(@p0,@p1,@p2);";
 	$result=array();

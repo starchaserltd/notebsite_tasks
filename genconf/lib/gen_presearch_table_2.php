a3a7a31c5654095daf77e34277ca4705246b90e9
root <root@ip-172-31-8-200.ec2.internal>
2019-02-19 21:07:45 +0000

    fix

--- genconf/lib/gen_presearch_table_2.php
+++ genconf/lib/gen_presearch_table_2.php
@@ -9,7 +9,7 @@ if($result&&mysqli_num_rows($result)>0)
 
 	while($model=mysqli_fetch_assoc($result))
 	{			
-		$sql="SELECT max(`price`) as `max_price`,min(`price`) as `min_price`,max(`batlife`) as `max_batlife`,min(`batlife`) as `min_batlife`,max(`capacity`) as `max_cap`,min(`capacity`) as `min_cap` FROM `all_conf_".$model['model_id']."` WHERE `price`!=0 LIMIT 1";
+		$sql="SELECT max(`price`) as `max_price`,min(`price`) as `min_price`,max(`batlife`) as `max_batlife`,min(`batlife`) as `min_batlife`,max(`capacity`) as `max_cap`,min(`capacity`) as `min_cap` FROM `notebro_temp`.`all_conf_".$model['model_id']."` WHERE `price`!=0 LIMIT 1";
 		$result_price=mysqli_query($cons,$sql);
 		if($result_price&&mysqli_num_rows($result_price)>0)
 		{ $row=mysqli_fetch_assoc($result_price);

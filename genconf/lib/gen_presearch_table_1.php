d57c53e06b29e62abb4b0564fca856843874914c
root <root@ip-172-31-8-200.ec2.internal>
2019-02-20 09:46:34 +0000

    added pmodel

--- genconf/lib/gen_presearch_table_1.php
+++ genconf/lib/gen_presearch_table_1.php
@@ -1,4 +1,5 @@
 <?php
+
 if ($result = mysqli_query($con, "SELECT DATABASE()")) {
     $row = mysqli_fetch_row($result);
     printf("Default database is %s.\n", $row[0]);
@@ -14,14 +15,14 @@ if(mysqli_multi_query($cons,"CALL `presearch_tbl`();"))
 } 
 mysqli_select_db($con,"notebro_db");
 
-$sql="SELECT `id`,`cpu`,`display`,`mem`,`hdd`,`shdd`,`gpu`,`wnet`,`odd`,`mdb`,`chassis`,`acum`,`warranty`,`sist` FROM `notebro_db`.`MODEL`";
+$sql="SELECT `id`,`cpu`,`display`,`mem`,`hdd`,`shdd`,`gpu`,`wnet`,`odd`,`mdb`,`chassis`,`acum`,`warranty`,`sist`,`p_model` FROM `notebro_db`.`MODEL`";
 $result=mysqli_query($con,$sql);
 
 if($result&&mysqli_num_rows($result)>0)
 {
 	while($row=mysqli_fetch_assoc($result))
 	{	
-		$insert_sql="INSERT INTO `notebro_temp`.`presearch_tbl` (`model_id`,`cpu`,`display`,`mem`,`hdd`,`shdd`,`gpu`,`wnet`,`odd`,`mdb`,`chassis`,`acum`,`war`,`sist`,`min_price`) VALUES ('".$row["id"]."','".$row["cpu"]."','".$row["display"]."','".$row["mem"]."','".$row["hdd"]."','".$row["shdd"]."','".$row["gpu"]."','".$row["wnet"]."','".$row["odd"]."','".$row["mdb"]."','".$row["chassis"]."','".$row["acum"]."','".$row["warranty"]."','".$row["sist"]."','0')";
+		$insert_sql="INSERT INTO `notebro_temp`.`presearch_tbl` (`model_id`,`cpu`,`display`,`mem`,`hdd`,`shdd`,`gpu`,`wnet`,`odd`,`mdb`,`chassis`,`acum`,`war`,`sist`,`p_model`,`min_price`) VALUES ('".$row["id"]."','".$row["cpu"]."','".$row["display"]."','".$row["mem"]."','".$row["hdd"]."','".$row["shdd"]."','".$row["gpu"]."','".$row["wnet"]."','".$row["odd"]."','".$row["mdb"]."','".$row["chassis"]."','".$row["acum"]."','".$row["warranty"]."','".$row["sist"]."','".$row["p_model"]."','0')";
 		if(!mysqli_query($cons,$insert_sql)){echo("Error description: ".mysqli_error($cons)." ".$insert_sql."<br>");}
 	}
 }

1e4b43a8a3f38e78c83a4b586565f8c5c8f99e9e
root <root@ip-172-31-8-200.ec2.internal>
2019-09-25 15:06:14 +0000

    changed rating vars

--- genconf/lib/var_conf.php
+++ genconf/lib/var_conf.php
@@ -3,12 +3,12 @@
 // SETTING VARIABLES FOR EASY EDITING AT LATER TIME
 $boost_i=1.1;
 
-$cpu_i=11*$boost_i;
+$cpu_i=12*$boost_i;
 $display_i=12*$boost_i;
-$mem_i=7*$boost_i;
-$hdd_i=7*$boost_i;
-$shdd_i=4*$boost_i;
-$gpu_i=12*$boost_i;
+$mem_i=8*$boost_i;
+$hdd_i=5*$boost_i;
+$shdd_i=3*$boost_i;
+$gpu_i=13*$boost_i;
 $wnet_i=4*$boost_i;
 $odd_i=3*$boost_i;
 $mdb_i=11*$boost_i;
